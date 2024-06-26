425d2d2621329e10e34e785cd9cef3c3b46c903f
KapDarIA <roopr7966@gmail.com>
2024-06-27 02:53:23 +0300

    Add files via upload

---
+++ LabWork11/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿List<string> valutes = new List<string>() { "euro", "ruble", "dollar" };
+Console.Write("Введите количество валют, которые хотите добавить:");
+int valutesCount = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < valutesCount; i++)
+{
+    valutes.Add(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < valutes.Count; i++)
+{
+    Console.WriteLine($"{i + 1}-{valutes[i]}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Введите первую и вторую позиции:");
+int position1 = Convert.ToInt32(Console.ReadLine()) - 1;
+int position2 = Convert.ToInt32(Console.ReadLine()) - 1;
+
+if (position1 < 0 || position2 > valutes.Count - 1)
+{
+    Console.WriteLine("некорректныне данные");
+}
+else
+{
+    for (int i = position1; i <= position2; i++)
+    {
+        Console.WriteLine($"Номер:{i + 1}-{valutes[i]}");
+    }
+}
+
+
---
+++ LabWork11/Task2/Program.cs
@@ -0,0 +1,78 @@
+﻿Dictionary<string, int> users = new Dictionary<string, int>()
+{
+    {"login123",3 },
+    {"XXX_Tolyan228_XXX2019",5 },
+    {"MasterOfBeer",40 }
+};
+
+Console.Write("Введите количество пользователей, которых хотите добавить:");
+int usersCount = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+for (int i = 0; i < usersCount; i++)
+{
+    Console.Write("Введите логин пользователя:");
+    string login = Console.ReadLine();
+
+    Console.Write("Введите возраст пользователя:");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    users.TryAdd(login, age);
+}
+
+Console.WriteLine();
+
+foreach (var user in users)
+{ 
+    Console.WriteLine($"Логин:{user.Key} - Возраст:{user.Value}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine($"Количество элементов в словаре:{users.Count}");
+
+Console.WriteLine();
+
+Console.Write("Введите ключ, который нужно найти:");
+string key = Console.ReadLine();
+
+if (users.ContainsKey(key))
+{
+    Console.WriteLine($"Пользователь найден. Информация о нём: Логин:{key} - Возраст:{users[key]}");
+}
+else
+{
+    Console.WriteLine("Ключ не найден");
+}
+
+Console.WriteLine();
+
+Console.Write("Введите возраст, у которого хотите найти количество совпадений:");
+int searchAge = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+foreach (var user in users)
+{
+    if (user.Value == searchAge)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество совпадений: {count}");
+
+Console.WriteLine();
+
+Console.Write("Введите логин элемента, который хотите удалить:");
+string deleteUser = Console.ReadLine();
+
+Console.WriteLine();
+
+if (users.ContainsKey(deleteUser))
+{
+    users.Remove(deleteUser);
+    Console.WriteLine("Пользователь удалён");
+}
+
+Console.WriteLine();
+foreach (var user in users) Console.WriteLine($"Логин:{user.Key} - Возраст:{user.Value}");
