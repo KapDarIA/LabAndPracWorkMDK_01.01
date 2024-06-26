11df59e7d10ce730a92b50f02f8afce2db76d9b0
KapDarIA <roopr7966@gmail.com>
2024-06-27 02:57:46 +0300

    Add files via upload

---
+++ LabWork13/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿int quantity = 0;
+
+Console.Write("Введите строку: ");
+string text = Console.ReadLine();
+Console.WriteLine($"Количество символов в данной строке: {text.Length}");
+
+for (int i = 0; i < text.Length; i++)
+{
+    if (Char.IsLetter(text[i]))
+        quantity++;
+}
+
+Console.WriteLine($"Количество символов без учёта пробелов: {text.Replace(" ", "").Length}");
+Console.WriteLine($"Количество букв во введенной пользователем строке: {quantity}");
+Console.Write("Введите символ для поиска: ");
+char symbol = Convert.ToChar(Console.ReadLine());
+
+for (int i = 0; i < text.Length; i++)
+    if (text[i] == symbol)
+        Console.WriteLine($"Искомый символ находится на позиции {i + 1}");
+if (!text.Contains(symbol))
+    Console.WriteLine("Символ не найден в данной строке");
---
+++ LabWork13/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+Console.Write("Введите строку:");
+string text = Console.ReadLine();
+Regex regex = new Regex(@"\s+");
+text = regex.Replace(text, @" ").Trim();
+Console.WriteLine($"Измененная строка:{text}");
+
+StringBuilder stringBuilder1 = new StringBuilder(text);
+StringBuilder stringBuilder2 = new StringBuilder(text);
+
+for (int i = 0; i < stringBuilder1.Length; i++)
+    stringBuilder1[i]= char.ToLower(stringBuilder1[i]);
+Console.WriteLine($"Текст в нижнем регистре:{stringBuilder1}");
+
+for (int i = 0;i < stringBuilder1.Length;i++)
+    stringBuilder1[i] = char.ToUpper(stringBuilder1[i]);
+Console.WriteLine($"Текст в верхнем регистре:{stringBuilder1}");
+
+for (int i = 0; i<stringBuilder2.Length;i++)
+{
+    if (char.IsLower(stringBuilder2[i]))
+        stringBuilder2[i] = char.ToUpper(stringBuilder2[i]);
+    else
+        stringBuilder2[i] = char.ToLower(stringBuilder2[i]);
+}
+
+
