47c0b2206742ba98b47142ff11ed35136d7f0d75
KapDarIA <roopr7966@gmail.com>
2024-06-27 00:45:15 +0300

    Add files via upload

---
+++ Labwork1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Сортировка методом простого выбора");
+
+int[] array = { 7, 0, -4, 3, 1, -2, 5 };
+int min, temp;
+
+for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]}\t");
+Console.WriteLine();
+
+for (int i = 0; i < array.Length - 1; i++)
+{
+    min = i;
+
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[j] < array[min])
+            min = j;
+    }
+
+    temp = array[i];
+    array[i] = array[min];
+    array[min] = temp;
+
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]}\t");
+    }
+    Console.WriteLine();
+}
+
---
+++ Labwork1/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Сортировка вставками");
+
+int[] array = { 7, 0, -4, 3, 1, -2, 5 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} \t");
+}
+
+Console.WriteLine();
+
+for (int i = 1; i < array.Length; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        if (array[j] > array[i])
+        {
+            (array[j], array[i]) = (array[i], array[j]);
+        }
+    }
+
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]} \t");
+    }
+
+    Console.WriteLine();
+}
+
