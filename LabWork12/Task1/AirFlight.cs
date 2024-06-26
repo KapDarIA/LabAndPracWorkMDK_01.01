0b23e3f16b23bc7ab59b43eaa1ed54861d36e71c
KapDarIA <roopr7966@gmail.com>
2024-06-27 02:55:46 +0300

    Add files via upload

---
+++ LabWork12/Task1/AirFlight.cs
@@ -0,0 +1,60 @@
+﻿namespace Task1
+{
+    internal struct AirFlight
+    {
+        private string _destination;
+        private int _number;
+        private int _capacity;
+        public Type Type;
+
+        public string Destination
+        {
+            get => _destination;
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _destination = value;
+                }
+            }
+        }
+
+        public int Number
+        {
+            get => _number;
+            set
+            {
+                if (value >= 0)
+                {
+                    _number = value;
+                }
+            }
+        }
+
+        public int Capacity
+        {
+            get => _capacity;
+            set
+            {
+                if (value >= 0)
+                {
+                    _capacity = value;
+                }
+            }
+        }
+
+        public AirFlight(string destination, int number, int capacity)
+        {
+            Destination = destination;
+            Number = number;
+            Capacity = capacity;
+            Type = Type.International;
+        }
+
+
+        public void Print()
+        {
+            Console.WriteLine($"Пункт назначения:{Destination}\nНомер рейса:{Number}\nВместимость:{Capacity}\nТип полета:{Type}");
+        }
+    }
+}
---
+++ LabWork12/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AirFlight airFlight1 = new AirFlight("Архангельск", 1, 100);
+            AirFlight airFlight2 = new AirFlight("Абу-Даби", 2, 250);
+            AirFlight airFlight3 = new AirFlight("Северодвинск", 3, 105);
+            AirFlight airFlight4 = new AirFlight("Санкт-Петербург", 4, 1000);
+
+            airFlight1.Print();
+
+            Console.WriteLine();
+
+            airFlight1.Number = 5;
+            airFlight1.Destination = "Новочеркаск";
+            airFlight1.Type = Type.Iterior;
+            airFlight1.Print();
+            airFlight3.Type= Type.Iterior;
+
+            Console.WriteLine("Какой тип рейса вам нужен\n1.Международный  2.Внутрений");
+            string findType = Console.ReadLine();
+
+            AirFlight[] flights = new AirFlight[] { airFlight1, airFlight2,airFlight3,airFlight4 };
+            for (int i = 0; i < flights.Length; i++) 
+            {
+                if(findType == Type.Iterior)
+            }
+        }
+    }
+}
