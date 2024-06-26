d43596e18fbdf13842342199699664b3c374c1d8
KapDarIA <roopr7966@gmail.com>
2024-06-27 02:47:01 +0300

    Add files via upload

---
+++ LabWork5/Task1/AirFlight.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class AirFlight
+    {
+        private string _destination;
+        private int _flightNumber;
+        private int _capacity;
+
+        public string Destination
+        {
+            get => _destination;
+
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _destination = value;
+                }
+            }
+        }
+
+        public int FlightNumber
+        {
+            get => _flightNumber;
+
+            set 
+            {
+                if (value >= 0)
+                {
+                    _flightNumber = value;
+                }
+            }
+        }
+        public int Capacity
+        {
+            get => _capacity;
+
+            set
+            {
+                if (value >= 0)
+                {
+                    _capacity = value;
+                }
+            }
+        }
+
+        public AirFlight() 
+        {
+            _destination = "Архангельск";
+            _flightNumber = 0;
+            _capacity = 0;
+        }
+
+        public AirFlight(string destination, int flightNumber, int capacity)
+        {
+            _destination = destination;
+            _flightNumber = flightNumber;
+            _capacity = capacity;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Пункт назначения: {_destination},\n" +
+                $"Номер рейса:{_flightNumber},\nВместимость:{_capacity}");
+        }
+    }
+}
---
+++ LabWork5/Task1/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Конструктор по умолчанию
+            AirFlight airFlight = new AirFlight(); 
+            airFlight.Print();
+
+            Console.WriteLine();
+
+            airFlight.Destination = "";
+            airFlight.FlightNumber = 7;
+            airFlight.Capacity = 100;
+            airFlight.Print();
+
+            Console.WriteLine();
+
+            // Конструктор с параметрами 
+            AirFlight airFlight2 = new AirFlight("Нью-Йорк", 1, 50);
+            airFlight2.Print();
+
+            Console.WriteLine();
+
+            airFlight2.Destination = "Сахалин";
+            airFlight2.Capacity = 200;
+            airFlight2.FlightNumber = 10;
+            airFlight2.Print();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите количество авиарейсов:");
+
+            int numberAirFlights = Convert.ToInt32(Console.ReadLine());
+
+            AirFlight[] airFlightListArray = new AirFlight[numberAirFlights];
+
+            string destination;
+            int flightNumber, capacity;
+
+            Console.WriteLine();
+
+            for (int i = 0; i < airFlightListArray.Length; i++)
+            {
+                Console.WriteLine("Введите пункт назначения:");
+                destination = Console.ReadLine();
+
+                Console.WriteLine("Введите номер рейса:");
+                flightNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите вместимость:");
+                capacity = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                airFlightListArray[i] = new AirFlight(destination, flightNumber, capacity);
+            }
+
+            for (int i = 0; i < airFlightListArray.Length; i++)
+            {
+                Console.WriteLine($"Авиарейс №{i+1}:");
+                airFlightListArray[i].Print();
+
+                Console.WriteLine();
+            }
+
+            //Вывод рейсов до заданного пункта назначения
+
+            Console.WriteLine("Введите нужный пункт назначения:");
+
+            destination = Console.ReadLine();
+            int errorCounter= 0;
+
+
+            for (int i = 0; i < airFlightListArray.Length; i++)
+            {
+                if (destination == airFlightListArray[i].Destination)
+                {
+                    airFlightListArray[i].Print();
+                }
+                else
+                {
+                    errorCounter++;
+                }
+            }
+
+            if (errorCounter == airFlightListArray.Length)
+            {
+                Console.WriteLine("Нет результатов поиска");
+            }
+
+            Console.WriteLine("Введите нужную вместимость:");
+            capacity = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < airFlightListArray.Length; i++)
+            {
+                if (capacity < airFlightListArray[i].Capacity)
+                {
+                    airFlightListArray[i].Print();
+                }
+                else
+                {
+                    errorCounter++;
+                }
+            }
+
+            if (errorCounter == airFlightListArray.Length)
+            {
+                Console.WriteLine("Нет результатов поиска");
+            }
+        }
+    }
+}
