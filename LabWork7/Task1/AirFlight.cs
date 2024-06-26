8a397f767f5d35a0bc48d0b255feb220c4eff77e
KapDarIA <roopr7966@gmail.com>
2024-06-27 02:48:29 +0300

    Add files via upload

---
+++ LabWork7/Task1/AirFlight.cs
@@ -0,0 +1,35 @@
+﻿namespace Task1
+{
+    internal class AirFlight
+    {
+        private string _destination { get; set; }
+        private int _flightNumber { get; set; }
+        private int _capacity { get; set; }
+
+        public AirFlight() : this("Москва", 1, -100) {}
+
+        public AirFlight(string destination, int flightNumber, int capacity)
+        {
+            _destination = destination;
+            _flightNumber = flightNumber;
+            _capacity = capacity;
+        }
+
+        public static AirFlight operator ++(AirFlight airFlight) => new AirFlight{_destination= airFlight._destination, _flightNumber = airFlight._flightNumber, _capacity = airFlight._capacity + 1};
+
+        public static AirFlight operator +(AirFlight a, AirFlight b) => new AirFlight{_destination = a._destination, _flightNumber = a._flightNumber, _capacity= a._capacity + b._capacity};
+
+        public static bool operator ==(AirFlight a, AirFlight b) => a._capacity == b._capacity && a._destination == b._destination && a._flightNumber == b._flightNumber;
+
+        public static bool operator !=(AirFlight a, AirFlight b) => a._capacity != b._capacity || a._destination != b._destination || a._flightNumber != b._flightNumber;
+
+        public static bool operator true(AirFlight airFlight) => airFlight._capacity >= 0;
+
+        public static bool operator false(AirFlight airFlight) => airFlight._capacity < 0;
+
+        public void Print()
+        {
+            Console.WriteLine($"Пункт назначения: {_destination}\nНомер рейса:{_flightNumber}\nВместимость:{_capacity}");
+        }
+    }
+}
---
+++ LabWork7/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Изначальные данные");
+            AirFlight airFlight1 = new AirFlight("Архангельск", 1, 20);
+            airFlight1.Print();
+            Console.WriteLine();
+            Console.WriteLine("Значения после оператора ++");
+            airFlight1++;
+            airFlight1.Print();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Изначальные данные");
+            AirFlight airFlight2 = new AirFlight("Москва", 1, -110);
+            airFlight2.Print();
+            Console.WriteLine();
+            Console.WriteLine("Значения после оператора ++");
+            airFlight2++;
+            airFlight2.Print();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Бинарный оператор +");
+            AirFlight airFlight3 = airFlight1 + airFlight2;
+            airFlight3.Print();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Оператор сравнения ==");
+            Console.WriteLine(airFlight1 == airFlight2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Оператор сравнения !=");
+            Console.WriteLine(airFlight1 != airFlight2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Унарный оператор true/false");
+            AirFlight airFlight4 = new AirFlight();
+            if (airFlight4) Console.WriteLine("true"); else Console.WriteLine("false");
+
+        }
+    }
+}
