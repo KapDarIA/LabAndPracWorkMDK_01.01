10d79e0f17695545e53eed97bbe35dd7a36e7e17
KapDarIA <roopr7966@gmail.com>
2024-06-27 02:51:34 +0300

    Add files via upload

---
+++ LabWork10/Task1/AirFlight.cs
@@ -0,0 +1,39 @@
+﻿namespace Task1
+{
+    internal class AirFlight : IComparable, IComparable<AirFlight>, IEquatable<AirFlight>
+    {
+        public string Destination {  get; }
+        public int FlightNumber { get; set;  }
+        public int Capacity { get; set; }
+
+        public AirFlight() : this("Архангельск", 1, 100) { }
+
+        public AirFlight(string destination, int flightNumber, int capacity)
+        {
+            Destination = destination;
+            FlightNumber = flightNumber;
+            Capacity = capacity;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Пункт назначения:{Destination}\nНомер рейса:{FlightNumber}\nВместимость:{Capacity}");
+        }
+        public int CompareTo(object? obj)
+        {
+            if (obj is AirFlight airFlight) { return Capacity.CompareTo(airFlight.Capacity); }
+            throw new NotImplementedException("Нет реализации");
+        }
+        public int CompareTo(AirFlight? airFlight)
+        {
+            if (airFlight is not null) { return Capacity.CompareTo(airFlight.Capacity); }
+            throw new NotImplementedException("Нет реализации");
+        }
+
+        public bool Equals(AirFlight? obj)
+        {
+            if (obj is AirFlight airFlight) { return Destination == airFlight.Destination && FlightNumber == airFlight.FlightNumber && Capacity == airFlight.Capacity; }
+            throw new NotImplementedException("Нет реализации");
+        }
+    }
+
+}
---
+++ LabWork10/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                AirFlight airFlight1 = new AirFlight();
+                AirFlight airFlight2 = new AirFlight("Москва", 2, 600);
+                AirFlight airFlight3 = new AirFlight("Питер", 3, 50);
+
+                Console.WriteLine(airFlight1.CompareTo(airFlight2));
+                Console.WriteLine(airFlight1.CompareTo(airFlight3)); 
+                Console.WriteLine(airFlight1.Equals(airFlight2));
+
+                Console.WriteLine();
+                
+
+                AirFlight[] arrayAirFlights = { airFlight1, airFlight2, airFlight3 };
+                Console.WriteLine("Изначальный массив:");
+                for (int i = 0; i < arrayAirFlights.Length; i++) { arrayAirFlights[i].Print(); Console.WriteLine(); }
+
+                Array.Sort(arrayAirFlights);
+                Array.Sort(arrayAirFlights);
+
+                Console.WriteLine("Измененный массив:");
+                for (int i = 0; i < arrayAirFlights.Length; i++) { arrayAirFlights[i].Print(); Console.WriteLine(); }
+            }
+            catch (NotImplementedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+}
