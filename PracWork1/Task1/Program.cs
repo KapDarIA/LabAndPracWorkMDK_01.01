094d239f710b8f921601a871e5e1e998303ccfe8
KapDarIA <roopr7966@gmail.com>
2024-06-28 00:12:37 +0300

    Add files via upload

---
+++ PracWork1/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя файла:");
+string fileName = Console.ReadLine();
+
+if (File.Exists(fileName))
+{
+    Console.WriteLine($"Содержимое файла:");
+    Console.WriteLine(File.ReadAllText(fileName));
+}
+else
+    Console.WriteLine($"папка {fileName} не существует");
+//C:\temp\ispp21\МДК 01.01
---
+++ PracWork1/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя файла:");
+string fileName = Console.ReadLine();
+
+
+if (File.Exists(fileName))
+    Console.WriteLine("Файл открыт на дозапись");
+else
+{
+    File.Create(fileName);
+    File.Move(fileName, "ПроверкаЗадания2");
+    Console.WriteLine($"Файл {fileName} создан и открыт на дозапись");
+}
+var text = Console.ReadLine();
+File.AppendAllText(fileName, text);
