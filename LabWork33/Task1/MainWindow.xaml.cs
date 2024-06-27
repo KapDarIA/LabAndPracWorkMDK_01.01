b773a57545908ca6f756cd5c8eadd5ee505b6f9c
KapDarIA <roopr7966@gmail.com>
2024-06-27 23:45:21 +0300

    Add files via upload

---
+++ LabWork33/Task1/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Game> games = new List<Game>()
+            {
+                new Game{IdGame=1,Name="Смута", Site="https://steam.com", Category="Симулятор диалогов",Price=2000},
+                 new Game{IdGame=2,Name="CS 2", Site="https://steam.com", Category="Shooter",Price=100},
+                  new Game{IdGame=3,Name="WoW", Site="https://steam.com", Category="RPG",Price=300},
+                   new Game{IdGame=4,Name="Супер корова", Site="https://steam.com", Category="RPG",Price=1000},
+                    new Game{IdGame=5,Name="Genshin", Site="https://steam.com", Category="Rogalic",Price=0}
+            };
+            GamesListView.ItemsSource = games;
+        }
+    }
+}
