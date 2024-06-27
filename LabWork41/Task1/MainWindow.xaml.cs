f7316cf00d89f01b45b72f48a54c0d8835df1467
KapDarIA <roopr7966@gmail.com>
2024-06-27 22:18:14 +0300

    Add files via upload

---
+++ LabWork41/Task1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Task1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuStackPanel.Width > 30)
+            {
+                DoubleAnimation menuAnimation = new DoubleAnimation();
+                menuAnimation.From = MenuStackPanel.ActualWidth;
+                menuAnimation.To = 26;
+                menuAnimation.Duration = TimeSpan.FromSeconds(1);
+                MenuStackPanel.BeginAnimation(StackPanel.WidthProperty, menuAnimation);
+            }
+            else
+            {
+                DoubleAnimation menuAnimation = new DoubleAnimation();
+                menuAnimation.From = MenuStackPanel.ActualWidth;
+                menuAnimation.To = 100;
+                menuAnimation.Duration = TimeSpan.FromSeconds(1);
+                MenuStackPanel.BeginAnimation(StackPanel.WidthProperty, menuAnimation);
+            }
+        }
+    }
+}
