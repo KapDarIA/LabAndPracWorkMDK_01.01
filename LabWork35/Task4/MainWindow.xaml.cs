b527c9aafa62c13cd783e040f9b10d8d84718bde
KapDarIA <roopr7966@gmail.com>
2024-06-28 00:00:13 +0300

    Add files via upload

---
+++ LabWork35/Task4/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace Task4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new()
+            {
+                Filter = "Картинки|*.png;*.jpg;*.jpeg;*.bmp",
+                InitialDirectory = Environment.CurrentDirectory,
+                Title = "Выбор изображений",
+                Multiselect = true
+            };
+
+            if (dialog.ShowDialog() != true)
+                return;
+            ImagesListView.ItemsSource = dialog.FileNames;
+        }
+    }
+}
