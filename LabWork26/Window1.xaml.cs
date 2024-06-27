7d07508df1f333e83924e263f325d45ebe73cdc0
KapDarIA <roopr7966@gmail.com>
2024-06-27 16:51:32 +0300

    Add files via upload

---
+++ LabWork26/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LabWork26
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Task1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Task2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Task3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Task4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Task5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
