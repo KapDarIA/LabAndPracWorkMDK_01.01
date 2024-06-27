fb22eb0fa92764436c8bcdff25b3c7ec8af446fc
KapDarIA <roopr7966@gmail.com>
2024-06-27 16:44:59 +0300

    Add files via upload

---
+++ LabWork21/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetDaysCount(int month, int year)
+{
+    int[] monthArray = { 31, (year % 400 == 0 || year % 100 != 0 && year % 4 == 0) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    if (month > 1 && month < 13)
+        return monthArray[month - 1];
+    else return -1;
+}
+
+Console.Write("Введите год:");
+int year = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите номер месяца:");
+int month = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetDaysCount(month, year));
+
---
+++ LabWork21/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿double GetPower(double x, int n)
+{
+    if (n == 0)
+        return 1;
+    if (n % 2 == 0)
+        return x * x * GetPower(x, n - 2);
+    if (n % 2 == 1)
+        return x * GetPower(x, n - 1);
+    return -1;
+}
+Console.WriteLine(GetPower(2, 5));
