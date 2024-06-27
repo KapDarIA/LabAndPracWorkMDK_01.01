3c07ecd4acb6e68244f530459b1e9421fd6b35d2
KapDarIA <roopr7966@gmail.com>
2024-06-27 23:50:24 +0300

    Add files via upload

---
+++ LabWork33/Task3/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Game> games = new List<Game>()
+            {
+                new Game{IdGame=1,Name="Смута", Site="https://steam.com", Category="Симулятор диалогов",Price=2000},
+                 new Game{IdGame=2,Name="CS 2", Site="https://steam.com", Category="Shooter",Price=100},
+                  new Game{IdGame=3,Name="WoW", Site="https://steam.com", Category="RPG",Price=300},
+                   new Game{IdGame=4,Name="Супер корова", Site="https://steam.com", Category="RPG",Price=1000},
+                    new Game{IdGame=5,Name="Genshin", Site="https://steam.com", Category="Rogalic",Price=0}
+            };
+
+            GameDataGrid.ItemsSource = games;
+        }
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
