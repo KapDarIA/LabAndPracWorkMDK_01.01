88dfff66cf34fc331dbbdfe315260720a259e2ce
KapDarIA <roopr7966@gmail.com>
2024-06-28 00:26:00 +0300

    Add files via upload

---
+++ PracWork6/Task1/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Game
+    {
+        public int IdGame { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Category
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PracWork6/Task1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Task1
+{
+
+    public partial class MainWindow : Window
+    {
+        public List<Game> Games { get; set; } = new List<Game>
+        {
+            new Game { IdGame = 1, Name = "Tetris", Category = "головоломка", Price = 150 },
+            new Game { IdGame = 2, Name = "Flappy Bird", Description = "игра про летучую птицу", Category = "платформер", Price = 10 },
+            new Game { IdGame = 3, Name = "Pac-man", Description = "игра про колобка", Category = "аркада", Price = 300 },
+            new Game { IdGame = 4, Name = "Arkanoid", Category = "аркада", Price = 400 },
+            new Game { IdGame = 5, Name = "Mario", Description = "игра про Марио", Category = "платформер", Price = 1000 },
+            new Game { IdGame = 6, Name = "Tetris2", Category = "головоломка", Price = 150 },
+            new Game { IdGame = 7, Name = "Flappy Bird2", Description = "игра про летучую птицу", Category = "платформер", Price = 10 },
+            new Game { IdGame = 8, Name = "Pac-man2", Description = "игра про колобка", Category = "аркада", Price = 300 },
+            new Game { IdGame = 9, Name = "Arkanoid2", Category = "аркада", Price = 400 },
+            new Game { IdGame = 10, Name = "Mario2", Description = "игра про Марио", Category = "платформер", Price = 1000 },
+        };
+
+        public List<Category> categories = new List<Game>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            gameDataGrid.ItemsSource = games;
+
+            gameTextBox.DataContext = games;
+        }
+    }
+}
