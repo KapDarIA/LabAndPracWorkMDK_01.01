e4e56e24c5ed33bbf8ddf47697179c5fe22cd372
KapDarIA <roopr7966@gmail.com>
2024-06-27 16:22:34 +0300

    Add files via upload

---
+++ LabWork37/Tak1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace Tak1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var path = @"C:\temp\ispp21";
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            FileInfo[] files = directory.GetFiles("*", SearchOption.AllDirectories);
+            //var result = files.Where().ToList();
+            resultDataGrid.ItemsSource = files.ToList();
+        }
+    }
+}
