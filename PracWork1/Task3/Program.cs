058641aefaf79336c603027f4bb4acb8beda4ae0
KapDarIA <roopr7966@gmail.com>
2024-06-28 00:14:14 +0300

    Add files via upload

---
+++ PracWork1/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя текстового файла:");
+var path = Console.ReadLine();
+DirectoryInfo directory = new(path);
+Console.Write("Введите текст для поиска:");
+string text = Console.ReadLine();
+
+if (directory.Exists)
+{
+    var lines = File.ReadAllLines(path);
+    foreach (var line in lines)
+        if (line == text)
+            Console.WriteLine(line);
+}
+else
+    Console.WriteLine("Файл не найден");
